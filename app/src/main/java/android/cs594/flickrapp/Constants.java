a0a802c0a815628d661b848adb00cf7b80d5ca87
Soumik <soumik.shah@gmail.com>
2015-05-08 12:00:35 -0700

    FlickrApp
    It fetches data from flickr through its api and shows it in listview and
    imageview.

---
+++ app/src/main/java/android/cs594/flickrapp/Constants.java
@@ -0,0 +1,21 @@
+package android.cs594.flickrapp;
+
+/**
+ * Created by soumik on 5/8/2015.
+ */
+
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "4d024933034911fb1722783c15ce0b31";
+    public static final String SECRET = "b015daa762558215";
+    //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+    public static final String RADIUS = "1";
+
+    public static final String LATITUDE = "40.7127837";
+    public static final String LONGITUDE = "-74.00594130000002";
+
+
+
+}
